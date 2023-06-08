007edfc8405f3a942530eeca642267fdc7566396
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-06-08 17:02:59 +0100

    tweak the description

--- DSLs/kubernetes/dsl/build.gradle.kts
+++ DSLs/kubernetes/dsl/build.gradle.kts
@@ -57,7 +57,7 @@ if (enableSigning) { // when added it expects signing keys to be configured
 
 version = project.properties["releaseVersion"] ?: "LOCAL"
 group = "dev.forkhandles"
-description = "Kotlin DSL for Kubernetes configs"
+description = "Kotlin DSL for Kubernetes manifests"
 
 publishing {
     publications {
