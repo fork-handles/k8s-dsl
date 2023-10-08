16e8e76b1335a6a43c5f8b7ba9ad9681bc0f98fc
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-10-09 00:10:52 +0100

    upgrade dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention")
-    id("de.fayard.refreshVersions").version("0.51.0")
+    id("de.fayard.refreshVersions").version("0.60.3")
 }
 
 rootProject.name = "k8s-dsl"
