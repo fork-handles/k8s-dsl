5dd5e46788188a36d0e86b04d32dd006d01df39d
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-12-16 12:32:55 +0100

    add Nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Nix flake for k8s-dsl";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell rec {
+        packages = with pkgs; [
+          direnv
+          temurin-bin-21
+        ];
+      };
+    });
+}
