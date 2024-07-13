9844a68691ca7036f0e3eb314cfdfc97385620d0
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-13 17:24:45 +0100

    upgrade Gradle to v8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
 }
 
 tasks.wrapper {
-    gradleVersion = "8.8"
-    distributionSha256Sum = "f8b4f4772d302c8ff580bc40d0f56e715de69b163546944f787c87abf209c961"
+    gradleVersion = "8.9"
+    distributionSha256Sum = "258e722ec21e955201e31447b0aed14201765a3bfbae296a46cf60b70e66db70"
     distributionType = Wrapper.DistributionType.ALL
 }
 
