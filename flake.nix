2184cbeceadf09f9b58934e6305e8d20f6eff9d1
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-01-08 10:01:14 +0000

    use the latest Flake Checker from Nixpkgs instead

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
         shellHook = ''
           # health checks for Nix flake inputs
-          nix run "github:DeterminateSystems/flake-checker/v0.2.8"
+          nix run https://flakehub.com/f/NixOS/nixpkgs/0.1#flake-checker
         '';
       };
     });
